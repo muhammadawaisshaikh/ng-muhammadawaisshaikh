b67cdfde26a011fe0da095e37af2e8fa5c2c1a19
Muhammad Awais <awaisshaikh94@gmail.com>
2024-12-29 04:49:24 +0500

    gsap animations on banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InfiniteScrollingItemsComponent } from "../infinite-scrolling-items/infinite-scrolling-items.component";
+import { GsapTextService } from '../../helpers/gsap/gsap-text.service';
 
 @Component({
   selector: 'app-banner',
@@ -7,8 +8,16 @@ import { InfiniteScrollingItemsComponent } from "../infinite-scrolling-items/inf
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
   skills: string[] = ['JavaScript', 'Typescript', 'Angular', 'NgRX', 'RxJS', 'ReactJS', 'Redux', 'Zustand', 'NodeJS', 'NestJS', 'Python', 'Ai', 'ML', 'DL', 'TanStack', 'UI', 'MicroFrontends', 'MicroServices', 'Architect', 'Software', 'Web', 'Engineering']
   hashtags: string[] = ['Tech Speaker', 'Mentor', 'Author', 'Opensource'];
 
+  constructor(
+    private gsapService: GsapTextService
+  ) {}
+
+  ngOnInit(): void {
+    this.gsapService.titleAnimation('.heading', 100, 2, 0);
+    this.gsapService.textAnimate('.sub-heading');
+  }
 }
