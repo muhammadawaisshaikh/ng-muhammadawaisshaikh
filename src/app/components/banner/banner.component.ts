2554f663e835115b16e3635cae87d8950c9448d4
Muhammad Awais <awaisshaikh94@gmail.com>
2024-12-29 04:20:53 +0500

    animated hashtags skills at bottom

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { InfiniteScrollingItemsComponent } from "../infinite-scrolling-items/infinite-scrolling-items.component";
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [InfiniteScrollingItemsComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
 export class BannerComponent {
+  skills: string[] = ['JavaScript', 'Typescript', 'Angular', 'NgRX', 'RxJS', 'ReactJS', 'Redux', 'Zustand', 'NodeJS', 'NestJS', 'Python', 'Ai', 'ML', 'DL', 'TanStack', 'UI', 'MicroFrontends', 'MicroServices', 'Architect', 'Software', 'Web', 'Engineering']
+  hashtags: string[] = ['Tech Speaker', 'Mentor', 'Author', 'Opensource'];
 
 }
---
+++ src/app/components/infinite-scrolling-items/infinite-scrolling-items.component.ts
@@ -0,0 +1,15 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infinite-scrolling-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './infinite-scrolling-items.component.html',
+  styleUrl: './infinite-scrolling-items.component.scss'
+})
+export class InfiniteScrollingItemsComponent{
+
+
+  ngOnInit() {}
+}
